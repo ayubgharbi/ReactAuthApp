d5cc8f5b1203d53f71bdfb1fa261672eb1d77e3c
ayoub gharbi <ayoub97ma@gmail.com>
2019-09-06 17:18:14 +0100

    render props for managing the logged in state and redirecting users

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react'
 import Registration from './auth/Registrations';
 
 export default class Home extends Component {
+    constructor(props){
+        super(props);
+
+        this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+    }
+
+    handleSuccessfulAuth(data){
+        this.props.handleLogin(data);
+        this.props.history.push("/dashboard");
+    }
+
     render() {
         return (
             <div>
                 <h1>Home</h1>
-                <h1>Home</h1>
-                <Registration/>
+                <h1>Status: {this.props.loggedInStatus}</h1>
+                <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
             </div>
         );
     }
-}
+} 
